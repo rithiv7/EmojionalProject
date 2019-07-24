305abc2e4b87b15eebb2dc73056fc50572b79b15
Rithi Vaithyanathan <rithivaithya07@gmail.com>
2019-07-24 16:46:52 -0400

    Initial Commit

---
+++ EmojionalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojionalProject
+//
+//  Created by Rithi Vaithyanathan on 7/24/19.
+//  Copyright © 2019 Rithi Vaithyanathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
